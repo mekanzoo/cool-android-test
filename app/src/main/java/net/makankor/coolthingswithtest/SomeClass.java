cca671162d36a4c083eebede4fd047f8b34af15b
Addy Darmawan <addy@icehousecorp.com>
2015-04-08 13:44:18 +0700

    Unit test configuration applied

---
+++ app/src/main/java/net/makankor/coolthingswithtest/SomeClass.java
@@ -0,0 +1,9 @@
+package net.makankor.coolthingswithtest;
+
+
+/**
+ * Created by addy on 4/8/15.
+ */
+public class SomeClass {
+    
+}
---
+++ app/src/test/java/net/makankor/coolthingswithtest/SomeClassTest.java
@@ -0,0 +1,12 @@
+package net.makankor.coolthingswithtest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SomeClassTest {
+
+    @Test
+    public void test1() {
+       Assert.assertEquals(2, 2);
+    }
+}
